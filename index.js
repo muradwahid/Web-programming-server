82321daaf3c371ea7e9140b5df0567f26fe36072
muradwahid <muradwahid334@gmail.com>
2022-10-27 15:54:40 +0600

    category route api created

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = 5000;
 const cors = require('cors');
 const categories = require('./data/category.json')
 const data=require('./data/data.json')
+app.use(cors());
+app.get("/categories", (req, res) => {
+  res.send(categories)
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
